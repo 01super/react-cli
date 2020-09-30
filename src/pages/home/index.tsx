2a7b4d4f9b7e62eedc7e7e58f30d55ad5dee129b
Miller Feng <miller.feng@songmao.tech>
2020-09-30 11:28:49 +0800

    feat: 添加typeRoots配置

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,21 +3,26 @@ import Mine from '@/pages/mine';
 
 class Home extends Component {
   state = {
-    title: 12
+    title: 12,
+    name: 23
   };
 
   handleClick = (): void => {
+    const b: P = {
+      name: 123
+    };
     const a = 23;
     if (a === 23) {
-      this.setState({ title: 88 });
+      this.setState({ title: 88, ...b });
     }
   };
 
   render(): JSX.Element {
-    const { title } = this.state;
+    const { title, name } = this.state;
     return (
       <section>
         <div onClick={this.handleClick}>{title}</div>
+        <div>{name}</div>
         <Mine />
       </section>
     );
