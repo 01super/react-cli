aa3e873e4e2f2a1dc82f182c753697b0608c4240
01super <fk1024@outlook.com>
2021-03-06 21:31:42 +0800

    fix: 修改wenpack配置

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -16,17 +16,14 @@ const developmentConfig = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
-    // open: true,
-    // host: '0.0.0.0', // 这样配置可以使其它设备在同一局域网中也能够访问到
+    open: 'http://localhost:8080',
+    host: '0.0.0.0', // 这样配置可以使其它设备在同一局域网中也能够访问到
     port: 8080,
     hot: true,
     stats: 'errors-only', // 编译时的输出
     historyApiFallback: true // 解决刷新时路由找不到页面报404
   },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new ReactRefreshWebpackPlugin()
-  ]
+  plugins: [new webpack.HotModuleReplacementPlugin(), new ReactRefreshWebpackPlugin()]
 };
 
 module.exports = merge(common, developmentConfig);
