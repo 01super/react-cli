586daad1245e3586288abd0e2bdce9bb9c5aecb0
fengkun <fengkun@longshine.com>
2021-08-03 18:00:56 +0800

    fix: 升级依赖，修改报错

--- .babelrc.js
+++ .babelrc.js
@@ -24,7 +24,7 @@ module.exports = {
         legacy: true
       }
     ],
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    ['@babel/plugin-proposal-class-properties', { loose: false }],
     '@babel/plugin-transform-runtime'
   ]
 };
